cd67f06fd5fae157068bd7706985157197ef680f
adamliu84 <adam.liu84@gmail.com>
2016-10-11 23:12:54 +0800

    Type 7 decryption

--- Cisco_pwdecrypt.hs
+++ Cisco_pwdecrypt.hs
@@ -8,8 +8,11 @@ import Data.ByteString.Lazy (toStrict, fromStrict)
 import Data.Digest.Pure.SHA (bytestringDigest, sha1)
 import Data.Char as C (isAlphaNum, isPunctuation)
 import Crypto.Cipher (makeKey, makeIV, cipherInit, DES_EDE3, cbcDecrypt)
+import Numeric (readHex)
+import Data.Bits (xor)
+import Data.Char (chr)
 
--- Testing crypted string with pure key reveal
+-- Testing crypted string with pure key reveal for enc_GroupPwd
 encsample1 = "9196FE0075E359E6A2486905A1EFAE9A11D652B2C588EF3FBA15574237302B74C194EC7D0DD16645CB534D94CE85FEC4" --letmein
 encsample2 = "A39CADD77ED72A9C75467D0F5A5C88BFCD75370DD63E3388D3F402AF50C4E5029071B0965C343B99B6D6636A8698562DDB2EE51020D87EA3" --HelloWorld
 encsample3 = "886E2FC74BFCD8B6FAF47784C386A50D0C1A5D0528D1E682B7EBAB6B2E91E792E389914767193F9114FA26C1E192034754F85FC97ED36509" --Th!sIsMyK3y#
@@ -40,6 +43,35 @@ decryptCiscoGroupPassword encrypttext = let bin_str = C8.group.fst.decode.C8.pac
                                             ctext = cbcDecrypt desede3 ivv (C8.concat enc)
                                         in C8.takeWhile (\x-> isAlphaNum x || isPunctuation x) ctext                                        
                                         where shadigest = C8.group.toStrict.bytestringDigest.sha1.fromStrict.C8.concat
+
+-- Testing crypted string with pure key reveal for Type 7
+type7sample1 = "046E1803362E595C260E0B240619050A2D" --UseYourOwnString
+type7sample2 = "01270E454822152238671D105A" --Th!sIsMyK3y#
+type7list = [type7sample1, type7sample2]
+    
+xlat :: [Int]    
+xlat = [0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f, 0x41, 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72, 0x6b, 0x6c,  0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53, 0x55, 0x42]
+
+getxlat :: Int -> Int -> String -> Char
+getxlat x y z = let a = xlat !! (x + y)
+                    b = fst $ (readHex(z)!!0)                    
+                    char = chr $ (xor a b)
+                in char
+                                        
+pairBy2 :: String -> [String]
+pairBy2 (x:y:zz) = [x:[y]] ++ pairBy2 zz
+pairBy2 [] = []
+                         
+decryptCiscoType7Password :: String -> String                         
+decryptCiscoType7Password x = let   index = read(take 2 x)::Int
+                                    enc_pwd = drop 2 x
+                                    pwd_hex = pairBy2 enc_pwd        
+                                    zpwd_hex = zip [0,1..] pwd_hex
+                                    cleartext = map (\(x,y) -> getxlat index x y) zpwd_hex
+                              in cleartext                   
+
 main = do
         print "enc_GroupPwd"
         mapM_ (putStrLn.C8.unpack.decryptCiscoGroupPassword) enclist
+        print "Type 7"
+        mapM_ (putStrLn.decryptCiscoType7Password) type7list
